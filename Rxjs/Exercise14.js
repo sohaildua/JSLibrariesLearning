4d64b4c0e43093ccd1015020d302ab313d766582
Restyled.io <commits@restyled.io>
2023-03-16 22:33:01 +0000

    Restyled by clang-format

--- Rxjs/Exercise14.js
+++ Rxjs/Exercise14.js
@@ -1,62 +1,55 @@
-import {
-    concatAll
-} from 'rxjs/operators';
+import {concatAll} from 'rxjs/operators';
 
 function exercise14() {
-    var movieLists = [{
-            name: "New Releases",
-            videos: [{
-                    "id": 70111470,
-                    "title": "Die Hard",
-                    "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
-                    "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-                    "rating": 4.0,
-                    "bookmark": []
-                },
-                {
-                    "id": 654356453,
-                    "title": "Bad Boys",
-                    "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
-                    "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-                    "rating": 5.0,
-                    "bookmark": [{
-                        id: 432534,
-                        time: 65876586
-                    }]
-                }
-            ]
+  var movieLists = [
+    {
+      name : "New Releases",
+      videos : [
+        {
+          "id" : 70111470,
+          "title" : "Die Hard",
+          "boxart" : "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+          "uri" : "http://api.netflix.com/catalog/titles/movies/70111470",
+          "rating" : 4.0,
+          "bookmark" : []
         },
         {
-            name: "Dramas",
-            videos: [{
-                    "id": 65432445,
-                    "title": "The Chamber",
-                    "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
-                    "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-                    "rating": 4.0,
-                    "bookmark": []
-                },
-                {
-                    "id": 675465,
-                    "title": "Fracture",
-                    "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
-                    "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-                    "rating": 5.0,
-                    "bookmark": [{
-                        id: 432534,
-                        time: 65876586
-                    }]
-                }
-            ]
+          "id" : 654356453,
+          "title" : "Bad Boys",
+          "boxart" : "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+          "uri" : "http://api.netflix.com/catalog/titles/movies/70111470",
+          "rating" : 5.0,
+          "bookmark" : [ {id : 432534, time : 65876586} ]
         }
-    ];
+      ]
+    },
+    {
+      name : "Dramas",
+      videos : [
+        {
+          "id" : 65432445,
+          "title" : "The Chamber",
+          "boxart" : "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+          "uri" : "http://api.netflix.com/catalog/titles/movies/70111470",
+          "rating" : 4.0,
+          "bookmark" : []
+        },
+        {
+          "id" : 675465,
+          "title" : "Fracture",
+          "boxart" : "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+          "uri" : "http://api.netflix.com/catalog/titles/movies/70111470",
+          "rating" : 5.0,
+          "bookmark" : [ {id : 432534, time : 65876586} ]
+        }
+      ]
+    }
+  ];
 
-    // ------------   INSERT CODE HERE!  -----------------------------------
-    // Use map and concatAll to flatten the movieLists in a list of video ids.
-    // ------------   INSERT CODE HERE!  -----------------------------------
-    console.log("heyyyy")
-    return movieLists.
-    map(movieList => movieList.videos.map(video => video.id)).
-    concatAll();
-
+  // ------------   INSERT CODE HERE!  -----------------------------------
+  // Use map and concatAll to flatten the movieLists in a list of video ids.
+  // ------------   INSERT CODE HERE!  -----------------------------------
+  console.log("heyyyy")
+  return movieLists.map(movieList => movieList.videos.map(video => video.id))
+      .concatAll();
 }
